b31482dfc81b805f04610faf181b3aed5103621d
Rafael Brandão <r.augusto.brandao@gmail.com>
2019-03-21 11:33:22 -0300

    Added splash and redirected to Home Screen

--- iOS-Faire-Take-Home-Assignment/AppDelegate.swift
+++ iOS-Faire-Take-Home-Assignment/AppDelegate.swift
@@ -15,7 +15,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let homeViewController = HomeViewController.init(nibName: nil, bundle: nil)
+        let nav = UINavigationController(rootViewController: homeViewController)
+        window!.rootViewController = nav
+        window!.makeKeyAndVisible()
         return true
     }
 
