49d49f7e3a175e6e8e1354af067c6908d3e475f6
Rafael Brandão <r.augusto.brandao@gmail.com>
2019-03-21 11:23:39 -0300

    Initial commit

---
+++ iOS-Faire-Take-Home-Assignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS-Faire-Take-Home-Assignment
+//
+//  Created by Rafael Brandão on 21/03/2019.
+//  Copyright © 2019 Rafael Brandão. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ iOS-Faire-Take-Home-AssignmentTests/iOS_Faire_Take_Home_AssignmentTests.swift
@@ -0,0 +1,34 @@
+//
+//  iOS_Faire_Take_Home_AssignmentTests.swift
+//  iOS-Faire-Take-Home-AssignmentTests
+//
+//  Created by Rafael Brandão on 21/03/2019.
+//  Copyright © 2019 Rafael Brandão. All rights reserved.
+//
+
+import XCTest
+@testable import iOS_Faire_Take_Home_Assignment
+
+class iOS_Faire_Take_Home_AssignmentTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
